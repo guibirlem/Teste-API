136d1d12bf3e4c33ed7337319223f1a5d5655eda
guibirlem <guisperto@hotmail.com>
2024-07-04 16:24:20 -0300

    PROGRAMA ATUALIZADO VERSÃO FINAL

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-async function getWeather() {
-    const city = document.getElementById('city').value;
+async function obterClima() {
+    const cidade = document.getElementById('cidade').value;
     const apiKey = '811c6fd4df54aaf328eba39b0cfd630f'; // Substitua por sua chave de API
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`;
+//lang pt br para as informaçoes virem em portugues
 
     try {
         const response = await fetch(url);
         const data = await response.json();
 
         if (data.cod === 200) {
-            const weather = `
-                <p><strong>Cidade:</strong> ${data.name}</p>
-                <p><strong>Temperatura:</strong> ${data.main.temp} °C</p>
-                <p><strong>Clima:</strong> ${data.weather[0].description}</p>
-                <p><strong>Umidade:</strong> ${data.main.humidity}%</p>
-            `;
-            document.getElementById('weather-result').innerHTML = weather;
+            document.getElementById('nome-cidade').textContent = data.name;
+            document.getElementById('temperatura').textContent = data.main.temp;
+            document.getElementById('descricao-clima').textContent = data.weather[0].description;
+            document.getElementById('umidade').textContent = data.main.humidity;
         } else {
-            document.getElementById('weather-result').innerHTML = `<p>${data.message}</p>`;
+            document.getElementById('resultado-clima').innerHTML = `<p>${data.message}</p>`;
         }
     } catch (error) {
-        document.getElementById('weather-result').innerHTML = `<p>Algo deu errado. Por favor, tente novamente mais tarde.</p>`;
+        document.getElementById('resultado-clima').innerHTML = `<p>Algo deu errado. Por favor, tente novamente mais tarde.</p>`;
     }
 }
